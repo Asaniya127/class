d735d19ddc4894fc3db2c6dcc6dcf389b176687c
Saniya Ahuja <122442269+Asaniya127@users.noreply.github.com>
2025-06-30 12:40:58 +0530

    Update practice1.java

--- practice1.java
+++ practice1.java
@@ -1,6 +1,6 @@
 public class practice1 {
     public static void main(String[] args) {
-        long a = 787;
+        long a = 2210990787L;
         System.out.println("My roll number is : " +a);
     }
 }
